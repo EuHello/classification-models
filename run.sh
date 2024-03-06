e34a07f33a3f11b4381849970743790546025d52
eug <18398188+EuHello@users.noreply.github.com>
2024-03-06 14:11:10 +0800

    Fix bug with zero args

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ db_file_path="./data/lung_cancer.db"
 if [ -f "$db_file_path" ]; then
   echo "DB file exists."
   else
-    echo "Downloading DB from $url."
+    echo "Downloading dataset from $url"
     wget -P "$local_dir" "$url"
 fi
 
@@ -19,24 +19,27 @@ if [ -z "$1" ]; then
   echo "Run pre-processing and models."
   python3 ./src/preprocess.py
   python3 ./src/model.py
-fi
+  exit 0
 
-
-if [ "$1" = "-p" ]; then
+elif [ "$1" = "-p" ]; then
   echo "-p provided. Preprocessing"
   python3 ./src/preprocess.py
+  exit 0
 
 elif [ "$1" = "-ta" ]; then
   echo "-ta provided. Running Tuning for Alpha in Neural Network"
   python3 ./src/model.py "$1"
+  exit 0
 
 elif [ "$1" = "-tl" ]; then
   echo "-ta provided. Running Tuning for Lambda in Neural Network"
   python3 ./src/model.py "$1"
+  exit 0
 
 elif [ "$1" = "-m" ]; then
   echo "-m provided. Running both models"
   python3 ./src/model.py "$1"
+  exit 0
 
 else
   echo "Unknown argument $1"
