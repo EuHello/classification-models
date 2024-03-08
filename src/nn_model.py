f8c9c121f0a1a6c53095764c3a41020e71537b6d
eug <18398188+EuHello@users.noreply.github.com>
2024-03-08 11:03:54 +0800

    Add evaluate model after tuning hyper params

--- src/nn_model.py
+++ src/nn_model.py
@@ -8,7 +8,6 @@ from tensorflow import keras
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-
 pd.set_option('display.width', 0)
 input_file = 'processed.csv'
 
@@ -39,10 +38,10 @@ def select_xy(data: pd.DataFrame):
 
 
 def model_builder(hp):
-    hp_units = hp.Int('units', min_value=8, max_value=256, step=8)
+    hp_units = hp.Int('units', min_value=8, max_value=128, step=8)
     hp_hidden = hp.Int('hidden', min_value=4, max_value=64, step=4)
     hp_learning_rate = hp.Choice('learning_rate', values=[1e-4, 5e-4, 1e-3, 5e-3, 8e-3, 1e-2, 5e-2, 8e-2, 1e-1, 5e-1])
-    hp_lambda = hp.Choice('l2_lambda', values=[1e-06, 1e-05, 5e-05, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 1.0])
+    hp_lambda = hp.Choice('l2_lambda', values=[1e-06, 5e-06, 1e-05, 5e-05, 1e-4, 5e-4, 1e-3, 1e-2, 1e-1, 1.0])
 
     model = Sequential([
         tf.keras.Input(shape=(18,)),
@@ -59,28 +58,32 @@ def model_builder(hp):
     return model
 
 
-def tune_model(X_train, y_train):
+def tune_model(X_train, y_train, X_test, y_test):
     tuner = kt.Hyperband(model_builder,
                          objective='val_accuracy',
-                         max_epochs=40,
+                         max_epochs=50,
                          factor=3,
-                         directory='my_dir',
-                         project_name='intro_to_kt',
+                         directory='nn_tuner',
+                         project_name='classification',
                          overwrite=False)
 
     stop_early = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
     tuner.search(X_train, y_train, epochs=50, validation_split=0.2, callbacks=[stop_early])
 
-    # Get the optimal hyperparameters
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
     print(f"""
-    The hyperparameter search is complete. The optimal number of units in the first densely-connected
-    layer is {best_hps.get('units')}, the optimal number of units in the second densely-connected
-    layer {best_hps.get('hidden')}, the optimal learning rate for the optimizer
-    is {best_hps.get('learning_rate')} and the optimal lambda for the L2 regularizer
-    is {best_hps.get('l2_lambda')}.
+    The hyperparameter search is complete. 
+    The optimal number of units in the first densely-connected layer is {best_hps.get('units')}, 
+    the optimal number of units in the second densely-connected layer {best_hps.get('hidden')}, 
+    the optimal learning rate for the optimizer is {best_hps.get('learning_rate')}, 
+    and the optimal lambda for the L2 regularizer is {best_hps.get('l2_lambda')}.
     """)
+
+    hypermodel = tuner.hypermodel.build(best_hps)
+    hypermodel.fit(X_train, y_train, epochs=50, validation_split=0.2, verbose=0)
+    eval_result = hypermodel.evaluate(X_test, y_test)
+    logging.info(f"Test Loss, Test Accuracy: {eval_result}")
 
 
 def main():
@@ -88,15 +91,13 @@ def main():
 
     logging.info("Loading inputs")
     data_loaded = load_inputs()
-
     data, target = select_xy(data_loaded)
-    X_train, X_cv, y_train, y_cv = train_test_split(data, target, test_size=0.10, random_state=1)
-    logging.info(f"Training and CV split complete. X_train shape = {X_train.shape}, y_train shape = {y_train.shape}, "
-                 f"X_cv shape = {X_cv.shape}, y_cv shape = {y_cv.shape}")
 
-    # TODO: model.fit already has val split!!! No need to split CV?
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1, random_state=1)
+    logging.info(f"Training test split. X_train shape = {X_train.shape}, y_train shape = {y_train.shape}, "
+                 f"X_test shape = {X_test.shape}, y_test shape = {y_test.shape}")
 
-    tune_model(data, target)
+    tune_model(X_train, y_train, X_test, y_test)
 
     return 0
 
